d280212b9ccbae62924885f65a18c07fb1fd9abb
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-05-10 15:50:20 -0300

    php header

--- form.php
+++ form.php
@@ -16,6 +16,6 @@ $asunto = 'Datos del formulario Suscripción Sorpresa!'
 mail($para, $asunto, utf8_decode($nombre, $apellido, $email, $cel, $postal, $domicilio, $entrega));
 
 /* Redireccion */
-header('location: <pages>pagar.html')
+header('location: https://javacafe-colucci.netlify.app/pages/sorpresa.html');
 
 ?>
