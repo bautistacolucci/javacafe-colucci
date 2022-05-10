7dfd765fd76faf9138a2b9d3d4fca7c63ec09e5b
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-05-09 22:46:53 -0300

    php y pagar

---
+++ form.php
@@ -0,0 +1,21 @@
+<?php
+/* Variables */
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
+$cel = $_POST['cel'];
+$postal = $_POST['postal'];
+$domicilio = $_POST['domicilio'];
+$entrega = $_POST['entrega'];
+
+/* Datos de envio */
+$para = 'bautitattoogmail.com'
+$asunto = 'Datos del formulario Suscripción Sorpresa!'
+
+/* Función */
+mail($para, $asunto, utf8_decode($nombre, $apellido, $email, $cel, $postal, $domicilio, $entrega));
+
+/* Redireccion */
+header('location:pagar.html')
+
+?>
