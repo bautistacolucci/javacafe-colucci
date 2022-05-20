dbe94ec149ca65e8fceb4dd4abb2fdb005626a3e
Juan Bautista Colucci <juanbautistacolucci@gmail.com>
2022-05-19 22:26:06 -0300

    php form

--- form.php
+++ form.php
@@ -7,15 +7,16 @@ $cel = $_POST['cel'];
 $postal = $_POST['postal'];
 $domicilio = $_POST['domicilio'];
 $entrega = $_POST['entrega'];
+$mensaje = $_POST['mensaje'];
 
 /* Datos de envio */
-$para = 'bautitattoogmail.com'
-$asunto = 'Datos del formulario Suscripción Sorpresa!'
+$para = 'bautitattoogmail.com';
+$asunto = 'Datos del formulario Suscripción Sorpresa!';
 
 /* Función */
-mail($para, $asunto, utf8_decode($nombre, $apellido, $email, $cel, $postal, $domicilio, $entrega));
+mail($para, $asunto, utf8_decode($nombre, $apellido, $email, $cel, $postal, $domicilio, $entrega, $mensaje));
 
 /* Redireccion */
-header('location:pages/pagar.html');
+header('location:./pages/pagar.html');
 
 ?>
